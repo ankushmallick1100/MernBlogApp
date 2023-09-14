82abc56a46db7c52a9932795af9b5161687bd902
Ankush Mallick <76246903+ankushmallick1100@users.noreply.github.com>
2023-09-14 17:50:54 +0530

    Routes & Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,17 @@
-
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+      </Routes>
+      <Footer/>
+    </div>
   )
 }
 
---
+++ frontend/src/components/HomePosts.jsx
@@ -0,0 +1,8 @@
+
+const HomePosts = () => {
+  return (
+    <div>HomePosts</div>
+  )
+}
+
+export default HomePosts
