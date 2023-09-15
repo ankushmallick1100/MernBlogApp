92c71269b246e859437dcdce7c41727bebf5428b
Ankush Mallick <76246903+ankushmallick1100@users.noreply.github.com>
2023-09-15 17:27:34 +0530

    Create post page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import PostDetails from "./pages/PostDetails"
+import CreatePost from "./pages/CreatePost"
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route exact path="/login" element={<Login/>}/>
         <Route exact path="/register" element={<Register/>}/>
         <Route exact path="/posts/post/:id" element={<PostDetails/>}/>
+        <Route exact path="/write" element={<CreatePost/>}/>
       </Routes>
     </div>
   )
