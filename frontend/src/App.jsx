f66609e92a59a414b731676f09ee0871a2009e46
Ankush Mallick <76246903+ankushmallick1100@users.noreply.github.com>
2023-09-18 20:12:52 +0530

    Profile page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Register from "./pages/Register"
 import PostDetails from "./pages/PostDetails"
 import CreatePost from "./pages/CreatePost"
 import EditPost from "./pages/EditPost"
-
+import Profile from "./pages/Profile"
 
 const App = () => {
   return (
@@ -17,6 +17,7 @@ const App = () => {
         <Route exact path="/posts/post/:id" element={<PostDetails/>}/>
         <Route exact path="/write" element={<CreatePost/>}/>
         <Route exact path="/edit/:id" element={<EditPost/>}/>
+        <Route exact path="/profile/:id" element={<Profile/>}/>
       </Routes>
     </div>
   )
---
+++ frontend/src/pages/Profile.jsx
@@ -0,0 +1,36 @@
+import Footer from "../components/Footer"
+import Navbar from "../components/Navbar"
+import ProfilePosts from "../components/ProfilePosts"
+
+const Profile = () => {
+    return (
+        <div>
+            <Navbar />
+            <div className="px-8 md:px-[200px] mt-8 md:flex-row flex flex-col-reverse md:items-start items-start">
+                <div className="flex flex-col md:w-[70%] w-full mt-8 md:mt-0">
+                    <h1 className="text-xl font-bold mb-4">Your posts:</h1>
+                    <ProfilePosts />
+                    <ProfilePosts />
+                    <ProfilePosts />
+                    <ProfilePosts />
+                </div>
+                <div className="md:sticky md:top-16 flex justify-start md:justify-end items-start md:w-[30%] w-full md:items-end">
+                    <div className="flex flex-col space-y-4 items-start">
+                        <h1 className="text-xl font-bold mb-4">Profile</h1>
+                        <input className="outline-none px-4 py-2 text-gray-500" placeholder="Your username" type="text" />
+                        <input className="outline-none px-4 py-2 text-gray-500" placeholder="Your email" type="email" />
+                        <input className="outline-none px-4 py-2 text-gray-500" placeholder="Your password" type="password" />
+
+                        <div className="flex items-center space-x-4 mt-8">
+                            <button className="text-white font-semibold bg-black px-4 py-2 hover:text-black hover:bg-gray-400">Update</button>
+                            <button className="text-white font-semibold bg-black px-4 py-2 hover:text-black hover:bg-gray-400">Delete</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default Profile
