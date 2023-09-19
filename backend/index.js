8258611e257a77848abf351798c06c13e88bc5c7
Ankush Mallick <ankushmallick7976@gmail.com>
2023-09-20 01:31:31 +0530

    Auth routes

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const mongoose  = require('mongoose')
 const app = express()
+const mongoose  = require('mongoose')
 const dotenv = require('dotenv')
+const authRoute = require('./routes/auth')
 
 // Database
 const connectDB = async()=>{
@@ -15,6 +16,8 @@ const connectDB = async()=>{
 
 // Middlewares
 dotenv.config()
+app.use(express.json())
+app.use("/api/auth",authRoute)
 
 app.listen(process.env.PORT, ()=> {
     connectDB()
