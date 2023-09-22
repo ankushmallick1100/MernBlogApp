aacad1c96d918dad9578da060536801a64830490
Ankush Mallick <ankushmallick7976@gmail.com>
2023-09-22 21:18:23 +0530

    Updating Frontend

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,18 +1,35 @@
 import { Link } from 'react-router-dom'
 import { BsSearch } from 'react-icons/bs'
+import { FaBars } from 'react-icons/fa'
+import { useState } from 'react'
+import Menu from './Menu'
 
 const Navbar = () => {
+  const [menu, setMenu] = useState(false)
+
+  const showMenu = () => {
+    setMenu(!menu)
+  }
+
   const user = false
+
   return (
     <div className="flex items-center justify-between px-6 md:px-[200px] py-4">
       <h1 className='text-lg md:text-xl font-extrabold'><Link to="/">Blogopedia</Link></h1>
       <div className="flex items-center justify-center space-x-0">
-        <p><BsSearch/></p>
-        <input className="outline-none px-3" placeholder='Search a post' type='text'/>
+        <p><BsSearch /></p>
+        <input className="outline-none px-3" placeholder='Search a post' type='text' />
       </div>
-      <div className='flex items-center justify-center space-x-2 md:space-x-4'>
+      <div className='hidden md:flex items-center justify-center space-x-2 md:space-x-4'>
         {user ? <h3><Link to="/write">Write</Link></h3> : <h3><Link to="/login">Login</Link></h3>}
-        {user ? <h3>Profile</h3> : <h3><Link to="/register">Register</Link></h3>}
+        {user ? <div onClick={showMenu}>
+          {menu ? <Menu/> : ""}
+          <p className='cursor-pointer relative'><FaBars /></p> 
+          </div>: <h3><Link to="/register">Register</Link></h3>}
+      </div>
+      <div onClick={showMenu} className='md:hidden text-lg'>
+        <p className='cursor-pointer relative'><FaBars /></p>
+        {menu ? <Menu/> : ""}
       </div>
     </div>
   )
