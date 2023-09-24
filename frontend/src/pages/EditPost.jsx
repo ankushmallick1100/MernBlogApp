a2bf1230017f32d953eb27aa479387a2ea89c7d1
Ankush Mallick <ankushmallick7976@gmail.com>
2023-09-25 01:20:41 +0530

    Edit Posts

--- frontend/src/pages/EditPost.jsx
+++ frontend/src/pages/EditPost.jsx
@@ -1,12 +1,74 @@
-import { useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
 import { ImCross } from 'react-icons/im'
+import { URL } from "../url"
+import { useNavigate, useParams } from "react-router-dom"
+import axios from "axios"
+import { UserContext } from "../context/UserContext"
 
 const EditPost = () => {
 
+    const [title,setTitle] = useState("")
+    const [desc,setDesc] = useState("")
+    const [file,setFile] = useState("")
     const [cat, setCat] = useState('')
     const [cats, setCats] = useState([])
+
+    const postId = useParams().id
+    const {user} = useContext(UserContext)
+    const navigate = useNavigate()
+
+    const fetchPost = async() => {
+        try{
+            const res = await axios.get(URL + "/api/posts/" + postId)
+            setTitle(res.data.title)
+            setDesc(res.data.desc)
+            setFile(res.data.photo)
+            setCats(res.data.categories)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const handleUpdate = async (e) => {
+        e.preventDefault()
+        const post = {
+            title,
+            desc,
+            username: user.username,
+            userId: user._id,
+            categories: cats
+        }
+
+        if (file) {
+            const data = new FormData()
+            const filename = Date.now() + file.name
+            data.append("img", filename)
+            data.append("file", file)
+            post.photo = filename
+            // console.log(data)
+
+            // Img upload
+            try {
+                const imgUpload = await axios.post(URL + "/api/upload", data)
+                // console.log(imgUpload.data)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+
+        // console.log(post)
+
+        // Post upload
+        try {
+            const res = await axios.put(URL + "/api/posts/" + postId, post, {withCredentials:true})
+            navigate("/posts/post/" + res.data._id)
+            // console.log(res.data)
+        } catch (err) {
+            console.log(err)
+        }
+    }
 
     const addCategory = () => {
         let updatedCats = [...cats]
@@ -21,17 +83,22 @@ const EditPost = () => {
         setCats(updatedCats)
     }
 
+    useEffect(()=> {
+        fetchPost()
+    },[postId])
+
+
     return (
         <div>
             <Navbar />
             <div className="px-6 md:px-[200px] mt-8">
                 <h1 className="font-bold md:text-2xl text-xl">Update a post</h1>
                 <form className="w-full flex flex-col space-y-4 md:space-y-8 mt-4">
-                    <input type="text" placeholder="Enter post title" className="outline-none px-4 py-2 " />
-                    <input type="file" className="px-4" />
+                    <input onChange={(e)=> setTitle(e.target.value)} value={title} type="text" placeholder="Enter post title" className="outline-none px-4 py-2 " />
+                    <input onChange={(e)=> setFile(e.target.files[0])} type="file" className="px-4" />
                     <div className="flex flex-col">
                         <div className="flex items-center space-x-4 md:space-x-8">
-                            <input value={cat} onChange={(e) => setCat(e.target.value)} className="outline-none px-4 py-2" type="text" placeholder="Enter post category" />
+                            <input onChange={(e) => setCat(e.target.value)} value={cat} className="outline-none px-4 py-2" type="text" placeholder="Enter post category" />
                             <div onClick={addCategory} className="bg-black text-white px-4 py-2 font-semibold cursor-pointer">Add</div>
                         </div>
 
@@ -46,8 +113,8 @@ const EditPost = () => {
                         </div>
                     </div>
 
-                    <textarea rows={15} cols={30} className="px-4 py-3 outline-none" placeholder="Enter post description" />
-                    <button className="w-full md:w-[20%] mx-auto text-white bg-black font-semibold px-4 py-2 md:text-xl text-lg">Update</button>
+                    <textarea onChange={(e)=> setDesc(e.target.value)} value={desc} rows={15} cols={30} className="px-4 py-3 outline-none" placeholder="Enter post description" />
+                    <button onClick={handleUpdate} className="w-full md:w-[20%] mx-auto text-white bg-black font-semibold px-4 py-2 md:text-xl text-lg">Update</button>
                 </form>
             </div>
             <Footer />
