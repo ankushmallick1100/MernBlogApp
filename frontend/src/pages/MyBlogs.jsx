ed11031cc15559c47828a6247a8e74369421b40d
Ankush Mallick <ankushmallick7976@gmail.com>
2023-09-26 01:53:44 +0530

    MernBlogApp Complete

--- frontend/src/pages/MyBlogs.jsx
+++ frontend/src/pages/MyBlogs.jsx
@@ -1,15 +1,63 @@
+import HomePosts from "../components/HomePosts"
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
+import Loader from '../components/Loader'
+import axios from "axios"
+import { URL } from "../url"
+import { useContext, useEffect, useState } from "react"
+import { useLocation, Link } from "react-router-dom"
+import { UserContext } from "../context/UserContext"
 
 const MyBlogs = () => {
-  return (
-    <div>
-        <Navbar/>
+    const { search } = useLocation()
+    // console.log(search)
+    const [posts, setPosts] = useState([])
+    const [noResults, setNoResults] = useState(false)
+    const [loader, setLoader] = useState(false)
+    const { user } = useContext(UserContext)
+    // console.log(user)
 
+    const fetchPosts = async () => {
+        setLoader(true)
+        try {
+            const res = await axios.get(URL + "/api/posts/user/" + user._id)
+            setPosts(res.data)
+            // console.log(res.data)
+            if (res.data.length === 0) {
+                setNoResults(true)
+            }
+            else {
+                setNoResults(false)
+            }
+            setLoader(false)
+        } catch (err) {
+            console.log(err)
+            setLoader(true)
+        }
+    }
 
-        <Footer/>
-    </div>
-  )
+    useEffect(() => {
+        fetchPosts()
+
+    }, [search])
+
+    return (
+        <div>
+            <Navbar />
+
+            <div className="px-8 md:px-[200px] min-h-[80vh]">
+                {loader ? <div className="h-[40vh] flex justify-center items-center"><Loader /></div> : !noResults ? posts.map((post) => (
+                    <>
+                        <Link to={user ? `/posts/post/${post._id}` : '/login'}>
+                            <HomePosts key={post._id} post={post} />
+                        </Link>
+                    </>
+                )) : <h3 className="text-center font-bold mt-16">No posts available</h3>}
+            </div>
+
+            <Footer />
+        </div>
+    )
 }
 
 export default MyBlogs
