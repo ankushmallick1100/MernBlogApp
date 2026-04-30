2dc1bff3893216fb5197dbcd2eae92f5009ed883
ankushmallick1100 <ankushmallick7976@gmail.com>
2026-04-30 13:45:45 +0530

    New Version

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Blog Website
 <br>
-• This website is based on the MERN stack, where users can create and post their blogs so that other users can comment on that blog post. They can edit or delete their comments as well. <br>
-• A user can view their posted blog by navigating to their profiles, where they can also update their email and password. 
-• In Frontend, HTML, CSS - TailWind CSS framework and ReactJS JavaScript framework, and in the backend, NodeJS and ExpressJS are used to develop this website. <br>
-• MongoDB Atlas is used as a database for storing data in the cloud or retrieving it from the cloud. <br>
+• This blog website allows users to create blogs, edit and delete blogs. In addition, they can comment to their blogs and other users blogs as well.
+• They can search to other users blogs by the title of a blog in the search box
+• A user can check their posted blogs by going to their profile page where they can change their email address and password also.
+• This website is made with HTML, CSS - TailWind CSS, and ReactJS for the frontend and NodeJS and
+ExpressJS for the backend.
+• MongoDB which a NoSQL database, is used to store and retrieve data and information.
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const mongoose  = require('mongoose')
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
@@ -13,7 +13,7 @@ const commentRoute = require('./routes/comments')
 
 
 // Database
-const connectDB = async()=>{
+const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URL)
         console.log("Database is connected successfully.")
@@ -26,31 +26,31 @@ const connectDB = async()=>{
 dotenv.config()
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({origin:"http://localhost:5173", credentials:true}))
-app.use("/api/auth",authRoute)
-app.use("/api/users",userRoute)
-app.use("/api/posts",postRoute)
-app.use("/api/comments",commentRoute)
-app.use("/images",express.static(path.join(__dirname,"/images")))
+app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use("/api/auth", authRoute)
+app.use("/api/users", userRoute)
+app.use("/api/posts", postRoute)
+app.use("/api/comments", commentRoute)
+app.use("/images", express.static(path.join(__dirname, "/images")))
 
 // Image upload
 const storage = multer.diskStorage({
-    destination:(req, file, fn) => {
-        fn(null,"images")
+    destination: (req, file, fn) => {
+        fn(null, "images")
     },
-    filename:(req, file, fn) => {
+    filename: (req, file, fn) => {
         fn(null, req.body.img)
         // fn(null, 'image.jpg')
     }
 })
 
-const upload = multer({storage:storage})
-app.post("/api/upload", upload.single("file"), (req,res) => {
+const upload = multer({ storage: storage })
+app.post("/api/upload", upload.single("file"), (req, res) => {
     // console.log(req.body)
     res.status(200).json("Image has been uploaded successfully!")
 })
 
-app.listen(process.env.PORT, ()=> {
+app.listen(process.env.SERVER_PORT, () => {
     connectDB()
-    console.log("App is running on port " + process.env.PORT)
+    console.log("App is running on port " + process.env.SERVER_PORT)
 })
